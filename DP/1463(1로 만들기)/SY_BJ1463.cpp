72097de12d5fa90a80bc76f54bf4cd6b2932601b
syli9526 <syli4800@gmail.com>
2019-08-21 15:26:17 +0900

    syl code

--- DP/1463(1로 만들기)/SY_BJ1463.cpp
+++ DP/1463(1로 만들기)/SY_BJ1463.cpp
@@ -24,3 +24,40 @@ int main(void)
     printf("%d\n", f(n));
     return 0;
 }
+
+/*
+ 
+ //
+ //  main.cpp
+ //  BJ1463
+ //
+ //  Created by 소율 on 19/08/2019.
+ //  Copyright © 2019 소율. All rights reserved.
+ //
+ 
+ #include <iostream>
+ #include <vector>
+ 
+ using namespace std;
+ 
+ int N;
+ 
+ int main(int argc, const char * argv[]) {
+ 
+ scanf("%d",&N);
+ vector<int>dp(N+1);
+ fill(dp.begin(), dp.end(), 1<<9);
+ 
+ dp[1] = 0;
+ 
+ for (int i = 1; i < N; i++) {
+ dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+ if (i * 2 <= N) dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+ if (i * 3 <= N) dp[i * 3] = min(dp[i * 3], dp[i] + 1);
+ }
+ 
+ printf("%d",dp[N]);
+ 
+ }
+ 
+ */
